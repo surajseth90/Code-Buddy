146052b0d302693281871624a3d0d156fec8a02b
suraj gupta <surajseth90@gmail.com>
2022-07-31 16:13:54 +0530

    snack bar added

--- src/componants/HomePage/index.js
+++ src/componants/HomePage/index.js
@@ -1,18 +1,63 @@
 import { useState } from "react";
 import "./style.scss";
 import { v4 as uuidv4 } from "uuid";
+import { Snackbar, IconButton } from "@mui/material";
 
 function Home() {
   const [roomId, setRoomId] = useState("");
   const [userName, setUserName] = useState("");
+  const [snackbarState, setSnackbarState] = useState(false);
+  const [snackbarMsg, setSnackbarStateMsg] = useState("");
 
   const generateRoomId = () => {
     const id = uuidv4();
     setRoomId(id);
   };
 
+  const joinClickHandler = (e) => {
+    if (roomId === "" && userName === "") {
+      setSnackbarStateMsg("Please enter a valid room ID and user name");
+      setTimeout(() => {
+        setSnackbarState(true);
+      }, 100);
+    } else if (roomId === "" && userName !== "") {
+      setSnackbarStateMsg("Please enter a valid room ID");
+      setTimeout(() => {
+        setSnackbarState(true);
+      }, 100);
+    } else if (roomId !== "" && userName === "") {
+      setSnackbarStateMsg("Please enter a valid user name");
+      setTimeout(() => {
+        setSnackbarState(true);
+      }, 100);
+    } else {
+      console.log("joinClickHandler", e);
+    }
+  };
+
   return (
     <div className="home-page">
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        open={snackbarState}
+        autoHideDuration={2000}
+        onClose={() => {
+          setSnackbarState(false);
+        }}
+        message={<span id="message-id">{snackbarMsg}</span>}
+        action={[
+          <IconButton
+            key="close"
+            arial-label="Close"
+            color="inherit"
+            onClick={() => {
+              setSnackbarState(false);
+            }}
+          >
+            ×
+          </IconButton>,
+        ]}
+      />
       <div className="home-page-container">
         <input
           className="room-id"
@@ -33,7 +78,15 @@ function Home() {
           required
         />
         <div className="homepage-btn-container">
-          <button className="homepage-btn" type="submit">Join</button>
+          <button
+            className="homepage-btn"
+            type="submit"
+            onClick={(e) => {
+              joinClickHandler(e);
+            }}
+          >
+            Join
+          </button>
           <button className="homepage-btn" onClick={generateRoomId}>
             Create Room
           </button>
